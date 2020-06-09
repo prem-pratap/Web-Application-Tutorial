b0910a24bcf1ae402a7cd398c156ea413d976cc5
Prem Pratap Singh <pratapprem88@gmail.com>
2020-06-09 22:30:47 +0530

    Servlet File

---
+++ LoginServlet.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		String username=request.getParameter("uname");
+		String password=request.getParameter("pwd");
+		if(username.equals("Admin") && password.equals("admin@123")) {
+			out.println("<font color=green><b>Welcome Admin</b></font>");//this function can use html inline tags 
+		}
+		else {
+			out.println("<font color=red><b>Invalid username and password</b></font>");
+		}
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
