0a48b59ae2aa842619731cafa345443d56ce772f
Prem-pratap <pratapprem88@gmail.com>
2020-06-18 18:20:41 +0530

    Userbean Package

---
+++ com/wipro/bean/UserBean.java
@@ -0,0 +1,24 @@
+package com.wipro.bean;
+
+public class UserBean {
+	private String username;
+	private String password;
+	
+	public UserBean() {}
+	
+	public String getUserName() {
+		return this.username;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public void setUserName(String username) {
+		this.username=username;
+	}
+	
+	public void setPassword(String password) {
+		this.password=password;
+	}
+}
