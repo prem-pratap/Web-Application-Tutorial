03b582f48583c1f82213f2dd56571854ea731478
Prem-pratap <pratapprem88@gmail.com>
2020-06-30 22:36:54 +0530

    MVC user bean package

---
+++ JSP login with JDBC/com/wipro/bean/UserBean.java
@@ -0,0 +1,30 @@
+package com.wipro.bean;
+
+public class UserBean {
+	private String username;
+	private String password;
+	
+	public UserBean() {
+		
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setUsername(String uname) {
+		this.username=uname;
+	}
+	
+	public void setPassword(String pwd) {
+		this.password=pwd;
+	}
+	
+	
+		
+	
+}
