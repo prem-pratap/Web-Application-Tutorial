b2c73a9b5a59b63a6c1884e59493d80d55f9a6e7
Prem-pratap <pratapprem88@gmail.com>
2020-07-01 16:48:49 +0530

    DAO package for Data Access

---
+++ JSP login with JDBC/com/wipro/dao/UserDAO.java
@@ -0,0 +1,31 @@
+package com.wipro.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.wipro.bean.UserBean;
+import com.wipro.util.DBUtil;
+
+public class UserDAO {
+
+	public String checkUser(UserBean user) {
+		String status="Fail";
+		try {
+			String sql="select * from user_table where username=? and password=?";
+			Connection con=DBUtil.getConnection();
+			PreparedStatement pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, user.getUsername());
+			pstmt.setString(2, user.getPassword());
+			ResultSet rset=pstmt.executeQuery();
+			if(rset.next())
+				status="Success";
+			pstmt.close();
+			con.close();
+		}
+		catch(Exception e) {e.printStackTrace();}
+		
+		return status;
+		
+	}
+}
